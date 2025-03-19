34a19fce88fa9a69adcedefbbf7a5a9adfbad6cf
Muhammad Sufyan Siddiqui <112391971+sufyansidd19@users.noreply.github.com>
2025-03-19 16:47:52 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import {createBrowserRouter,RouterProvider} from "react-router-dom";
+import {AppLayout} from "./components/Layout/AppLayout";
+// import "./"
+
+import {Home} from "./pages/Home";
+import {About} from "./pages/About";
+import {Country} from "./pages/Country";
+import {Contact} from "./pagContact";
+import {ErrorPage} from "./pages/ErrorPage";
+
+
+
+
+
+
+
+
+
+
+
+
+
+const App=()=>{
+  return <h1>Hello React Project</h1>
+};
+export default App;
